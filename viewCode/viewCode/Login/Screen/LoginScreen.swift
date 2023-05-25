02e5912389ab7be2875dee2503906b3754726eb5
Eduardo Escaleira <duescaleira@gmail.com>
2023-05-25 15:34:20 -0300

    [UPDATE] added loginLabel and imageView elements to the loginScreen

--- viewCode/viewCode/Login/Screen/LoginScreen.swift
+++ viewCode/viewCode/Login/Screen/LoginScreen.swift
@@ -8,19 +8,29 @@
 import UIKit
 
 class LoginScreen: UIView {
-
+    
     lazy var loginLabel: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .white
+        label.textColor = .black
         label.font = UIFont.boldSystemFont(ofSize: 40)
         label.text = "Login"
         return label
     }()
     
+    lazy var logoAppImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "whatsapp")
+        image.tintColor = .green
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubView()
+        backgroundColor = .white
+        addSubview()
         setUpConstraints()
     }
     
@@ -28,22 +38,29 @@ class LoginScreen: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func addSubView() {
+    private func addSubview() {
         addSubview(loginLabel)
+        addSubview(logoAppImageView)
+        
     }
     
     private func setUpConstraints() {
         NSLayoutConstraint.activate([
-        
-            loginLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            loginLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
             
-        
+            loginLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 30),
+            loginLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            logoAppImageView.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 30),
+            logoAppImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+            logoAppImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
+            logoAppImageView.heightAnchor.constraint(equalToConstant: 130),
+            
+            
         ])
         
         
     }
     
     
-
+    
 }
